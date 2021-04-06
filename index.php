7d7e94f4bae16f022730be83bfa3bad7179ad660
Hankro93 <hanna.kron@lokron.com>
2021-04-06 10:14:29 +0200

    la till ett textfält och knapp

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
-<!-- hej -->
 
-<!-- test -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Vårt Zoo</title>
 </head>
 <body>
     
 
 <h1>mm detta är vårat zoo</h1>
 
+<!-- Textfält med knapp-->
+<label for="text">Skriv här</label><br>
+<textarea name="text" id="" cols="30" rows="10"><?php echo $text;?></textarea><br>
+<input type="submit" name="submit" value="Submit">  
+<br>
+<!-- -->
+
 <?php
+
+$text = "";
+
 ini_set('display_errors', 1);
 
 ini_set('display_startup_errors', 1);
@@ -29,6 +37,10 @@ $query = "SELECT * FROM zoo.animals";
 foreach ($dbh->query($query) as $animals) {
 echo $animals['name'] . "<br/>";
 }
+
+
+/* Textfält */
+echo $text;
 
 
 ?>
